eb261af1696dff4087b598541a662dbb30dd1c90
Sergei <56161979+arkadev-sx@users.noreply.github.com>
2022-08-17 19:52:41 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,33 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     HelloWorld,
   },
-};
+  data() {
+    return {
+      a: 'a',
+      b: 'a',
+    }
+  },
+}
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+img {
+  background: #000;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
